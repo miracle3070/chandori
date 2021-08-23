55f8dab948f90991b418dd02e883d57faa33e2b7
miracle3070 <miracle3070@naver.com>
2021-08-23 23:20:09 +0900

    회원가입, 로그인, 로그아웃 기능 구현
    관련 페이지 CSS는 추후에 손 봐야 함
    
    2021-08-23
    한상우.

--- chandori/account/urls.py
+++ chandori/account/urls.py
@@ -5,4 +5,7 @@ app_name = 'account'
 
 urlpatterns = [
     path('', account.views.edit, name="edit"),
+    path('login/', account.views.login_view, name="login"),
+    path('logout/', account.views.logout_view, name="logout"),
+    path('signup/', account.views.signup_view, name="signup"),
 ]
